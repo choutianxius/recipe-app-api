18d61ec53c69604e52a69e0c3d0514f3104c200b
choutianxius <choutianxius@gmail.com>
2023-02-08 18:27:48 +0800

    Fixed linting.

--- app/core/tests/test_health_check.py
+++ app/core/tests/test_health_check.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
+
 class HealthCheckTests(TestCase):
     """Health check API tests."""
 
